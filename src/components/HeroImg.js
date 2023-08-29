a52261cd64b167058eb8b4c1a66ff82a1718d340
Aryan <kumararyan8005@gmail.com>
2023-08-29 17:59:51 +0530

    Sample commit

---
+++ src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroImgStyles.css"
+import React from 'react'
+import IntroImg from "../assets/intro-bg.jpg"
+import {Link} from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="IntroImg"/>
+      </div>
+      <div className="content">
+        <p> HI, I'M ARYAN KUMAR</p>
+        <h1>FULL STACK DEVELOPER</h1>
+        <div>
+            <Link to="https://drive.google.com/file/d/1O56cm41lzJ2GklSh43_hZZmoziGbgEQg/view?usp=drive_link" className="btn">Resume</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroImg
